8617e93092bf95de8a30845e6bdb9c4c4aba2c65
viktorlov <orlovva@ymail.com>
2023-07-17 15:03:29 +0300

    (l) 20.1 code review <<catalog/urls.py>>

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-
-from catalog.views import index, contacts
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts)
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('catalog.urls')),
+                  path('contacts/', include('catalog.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
