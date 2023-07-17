b83b9bf8066b1a6988a643bc592db2aa643c2156
viktorlov <orlovva@ymail.com>
2023-07-17 15:56:36 +0300

    (l) 20.1 code review <<catalog/models.py>>

--- catalog/models.py
+++ catalog/models.py
@@ -12,7 +12,21 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.title} {self.price}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
 
 class Category(models.Model):
     title = models.CharField(max_length=50, verbose_name='Название')
     description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
