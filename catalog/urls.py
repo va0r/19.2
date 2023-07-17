d1024efa0c7a6d714f9e458b2279037df308575f
viktorlov <orlovva@ymail.com>
2023-07-17 15:14:26 +0300

    Revert "(l) 20.1 code review <<catalog/urls.py>>"
    This reverts commit 8617e93092bf95de8a30845e6bdb9c4c4aba2c65.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,8 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from catalog.views import index, contacts
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('catalog.urls')),
-                  path('contacts/', include('catalog.urls'))
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', index),
+    path('contacts/', contacts)
+]
