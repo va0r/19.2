983ae135e9cf1c7a02e81cf3928e8e4187a2c108
viktorlov <orlovva@ymail.com>
2023-07-17 15:03:08 +0300

    (l) 20.1 code review <<catalog/models.py>>

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Изображение')
+    category = models.ForeignKey('catalog.Category', on_delete=models.SET_NULL, null=True, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
