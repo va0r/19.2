7500ca67f4a074c3870792b6cf2e3f787033ac42
viktorlov <orlovva@ymail.com>
2023-07-17 15:56:26 +0300

    (l) 20.1 code review <<catalog/admin.py>>

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 from catalog.models import Product, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'price', 'category']
+    list_filter = ('category',)
+    search_fields = ('title', 'description')
