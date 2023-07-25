9c6f92ac9304f86722bfd6d1454d7fd94cb18721
viktorlov <orlovva@ymail.com>
2023-07-25 12:59:39 +0300

    (l) code review "Задание 3"

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Note(models.Model):
     title = models.CharField(max_length=100, verbose_name='Заголовок')
     slug = models.CharField(max_length=250, **NULLABLE, verbose_name='Slug')
     content = models.TextField(max_length=1000, verbose_name='Содержание')
-    image = models.ImageField(upload_to='categories/', **NULLABLE, verbose_name='Изображение')
+    image = models.ImageField(upload_to='blog/', **NULLABLE, verbose_name='Изображение')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     cnt_views = models.IntegerField(default=0, verbose_name='Количество просмотров')
