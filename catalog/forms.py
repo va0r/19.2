a4f6c5d58fb69ce3c011e1fc6899c3ac4df812d0
viktorlov <orlovva@ymail.com>
2023-07-31 17:35:05 +0300

    (l) code review <<catalog/forms.py>>

--- catalog/forms.py
+++ catalog/forms.py
@@ -3,8 +3,35 @@ from django import forms
 from catalog.models import Product
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        """
+        Стилизация формы
+        """
+
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         # fields = '__all__'
         exclude = ('created_at', 'modified_at',)
+
+    def __check(self, field):
+        PROHIBITED_WORDS = ('бесплатно', 'биржа', 'дешево', 'казино', 'крипта',
+                            'криптовалюта', 'обман', 'полиция', 'радар')
+        cleaned_data = self.cleaned_data[field]
+        for word in PROHIBITED_WORDS:
+            if word in cleaned_data.lower():
+                raise forms.ValidationError(f'ОШИБКА: запрещенное слово <<{word}>>.'
+                                            f' Нельзя добавлять продукты с подобным названием или описанием.')
+        return cleaned_data
+
+    def clean_title(self):
+        self.__check('title')
+
+    def clean_description(self):
+        self.__check('description')
