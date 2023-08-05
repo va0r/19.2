56a7399a4644b5a2e09b8aabba5b6295e9450945
viktorlov <orlovva@ymail.com>
2023-08-05 16:52:52 +0300

    (l) code review <<urls.py>>

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,8 +12,7 @@ urlpatterns = [
     path('products/', ProductListView.as_view(), name='products'),  # все товары
     path('contacts/', ContactListView.as_view(), name='contacts'),  # контакты
 
-    path('categories/<int:pk>/products', CategoryProductListView.as_view(), name='category_products'),
-    # все товары по выбранной категории
+    path('categories/<int:pk>/products', CategoryProductListView.as_view(), name='category_products'),  # все товары по выбранной категории
     path('products/create/', ProductCreateView.as_view(), name='create_product'),  # создание товара
     path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),  # изменение товара
 
