45991299234a8067a9cb704fe77fc477fc3ab1f6
va0r <i@vialor.ru>
2023-08-11 15:35:59 +0300

    (l) code review <<forms.py>>

--- users/forms.py
+++ users/forms.py
@@ -14,9 +14,8 @@ class UserRegisterForm(StyleFormMixin, UserCreationForm):
 class UserProfileForm(StyleFormMixin, UserChangeForm):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar')
+        fields = ('email', 'password', 'first_name', 'last_name', 'phone', 'avatar')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.fields['password'].widget = forms.HiddenInput()
