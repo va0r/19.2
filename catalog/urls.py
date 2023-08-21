4d6e143c5117831ecdb3c466d73b12f6a24cfc96
va0r <i@vialor.ru>
2023-08-21 22:15:10 +0300

    (l) code review <<catalog/urls.py>>

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,19 +1,28 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import IndexListView, CategoryListView, ProductListView, CategoryProductListView, \
-    ContactListView, ProductCreateView, ProductUpdateView
+    ContactListView, ProductCreateView, ProductUpdateView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', IndexListView.as_view(), name='index'),  # главная
-    path('categories/', CategoryListView.as_view(), name='categories'),  # все категории
-    path('products/', ProductListView.as_view(), name='products'),  # все товары
-    path('contacts/', ContactListView.as_view(), name='contacts'),  # контакты
+    # главная
+    path('', IndexListView.as_view(), name='index'),
+    # все категории
+    path('categories/', CategoryListView.as_view(), name='categories'),
+    # все товары
+    path('products/', ProductListView.as_view(), name='products'),
+    # контакты
+    path('contacts/', ContactListView.as_view(), name='contacts'),
 
-    path('categories/<int:pk>/products', CategoryProductListView.as_view(), name='category_products'),  # все товары по выбранной категории
-    path('products/create/', ProductCreateView.as_view(), name='create_product'),  # создание товара
-    path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),  # изменение товара
-
+    # все товары по выбранной категории
+    path('categories/<int:pk>/products/', CategoryProductListView.as_view(), name='category_products'),
+    # создание товара
+    path('products/create/', ProductCreateView.as_view(), name='create_product'),
+    # просмотр товара
+    path('products/read/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='read_product'),
+    # изменение товара
+    path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
 ]
